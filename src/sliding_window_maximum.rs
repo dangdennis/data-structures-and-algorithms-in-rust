06e2cd326a4b6397e209cedd5fe543a37279ab04
Dennis Dang <dangggdennis@gmail.com>
2019-03-09 12:34:33 -0800

    Complete sliding window maximum

--- src/sliding_window_maximum.rs
+++ src/sliding_window_maximum.rs
@@ -14,27 +14,50 @@ pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
     
     // init the deque
     let mut deque: VecDeque<i32> = VecDeque::new();
-    
+    let mut max_index = 0;
+
     for i in 0..k {
-        // clean the deque of any values less than the currently selected one
-        if deque.len() > 0 && deque[0] < i - k {
-            println!("{}", deque[0]);
-            println!("{}", i);
+        // remove items that fall outside the window
+        if deque.len() > 0 && deque[0] < i - k + 1 {
+            dbg!(&deque);
+            deque.pop_front();
         } 
-        
+
+        // clean the deque of any values less than the currently selected one
+        while (deque.len() > 0) && nums[*deque.back().unwrap() as usize] < nums[i as usize] {
+          deque.pop_back();
+        }
+
+        // add the current i
         deque.push_back(i);
-        res.push_back(nums[i]);
-        
-        
-        // append the value to the deque
-        // add the 
+        if nums[max_index] < nums[i as usize] {
+            max_index = i as usize;
+        }
     }
-    
-    return nums
+
+    res.push(nums[max_index as usize]);
+
+    for i in k..nums.len() as i32 {
+        // remove items that fall outside the window
+        if deque.len() > 0 && deque[0] < i - k + 1 {
+            dbg!(&deque);
+            deque.pop_front();
+        } 
+
+        // clean the deque of any values less than the currently selected one
+        while (deque.len() > 0) && nums[*deque.back().unwrap() as usize] < nums[i as usize] {
+          deque.pop_back();
+        }
+
+        deque.push_back(i);
+
+        res.push(nums[deque[0] as usize]);
+    }
+    res
 }
 
 fn main() {
     let nums: Vec<i32> = vec![1,3,-1,-3,5,3,6,7];
     let k: i32 = 3;
-    max_sliding_window(nums, k);
+    let res = max_sliding_window(nums, k);
 }
